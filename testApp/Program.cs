bbc80cabfbdfb90a828849f60f0c4fcf9185f096
Nima0079 <nima.chamanpira@yahoo.com>
2023-12-06 09:13:03 +0330

    taghirat 1

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("iphone 15 pro mikham begiram ama register namishr ah ah");
 Console.WriteLine("auisodhuas das asuidh ashuiash asiuhd auishduai h");
+
+Console.WriteLine("taghirat");
